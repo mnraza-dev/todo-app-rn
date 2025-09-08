71aee5dc224dbee5f95ba6f2fcb5e58c226c484f
Md Noorullah Raza <mnraza94@gmail.com>
2025-09-08 11:34:20 +0530

    Refactor tab layout to enhance navigation and update icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,35 @@
-import { Tabs } from 'expo-router'
-import React from 'react'
-import { Text } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Text } from 'react-native';
 
 export default function TabLayout() {
     return (
         <Tabs screenOptions={{
-            headerShown: false
+            tabBarActiveTintColor: 'green',
+            tabBarInactiveTintColor: 'gray',
+            tabBarStyle: { 
+                height: 100, 
+                paddingTop: 10, 
+                paddingBottom: 30, 
+                backgroundColor: '#08062aff', 
+                borderTopWidth: 1, 
+                borderTopColor: 'yellow' 
+            },
+            tabBarLabelStyle: { fontSize: 12 , fontWeight: '600' },
+            headerShown: false,
+
         }}>
-            <Tabs.Screen name="index" options={{ title: "Home", tabBarIcon: () => <Text>🏠</Text> }} />
-            <Tabs.Screen name="settings" options={{ title: "Settings", tabBarIcon: () => <Text>⚙️</Text> }} />
+            <Tabs.Screen name="index" options={{
+                title: "Todos", tabBarIcon: ({ color, size }) => <Text>
+                    <Ionicons name="flash-outline" size={size} color={color} />
+                </Text>
+            }} />
+            <Tabs.Screen name="settings" options={{
+                title: "Settings", tabBarIcon: ({ color, size }) => <Text>
+                    <Ionicons name="settings" size={size} color={color} />
+                </Text>
+            }} />
 
         </Tabs>
     )
